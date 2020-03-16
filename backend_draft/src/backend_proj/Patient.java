ba4d42eeb5f25dd491262a137a112b33d17e0722
dylanleclair <45674837+dylanleclair@users.noreply.github.com>
2020-03-16 17:17:30 -0600

    Some more updates to the skeleton

--- backend_draft/src/backend_proj/Patient.java
+++ backend_draft/src/backend_proj/Patient.java
@@ -10,6 +10,8 @@ public class Patient extends User {
 	File patientRecord;
 	
 	
+	// if we want to be super extra we could hash appointments to an ID so that we can modify them 
+	
 	// I think we can save the patients records in a file / files that can be a subdirectory.
 	// bookings will be loaded and then differentiated
 	
