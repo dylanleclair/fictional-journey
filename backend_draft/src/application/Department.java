4f1b766d19e43b2e93ce53fc679a536e379cd12b
Fabian Aryo <fabianaryo@gmail.com>
2020-03-21 03:25:23 -0600

    Added More GUI for appointments and tests

---
+++ backend_draft/src/application/Department.java
@@ -0,0 +1,25 @@
+package application;
+
+
+/**
+ * Just an example. Make a toString for the GUI to be prettier. 
+ * @author Dylan Leclair
+ *
+ */
+public enum Department {
+	EMERGENCY,
+	CARDIOLOGY,
+	ICU,
+	IMAGING,
+	ENT, // Ear nose and throat
+	GASTROENTEROLOGY,
+	GENERAL_SURGERY,
+	MATERNITY,
+	NEUROLOGY,
+	NUTRITION,
+	OPHTALMOLOGY,
+	ORTHOPEDICS,
+	PHYSIOTHERAPY,
+	SEXUAL_HEALTH,
+	TBA
+}
