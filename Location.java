8ae752d4801d9a0290bc5327d93cccd313585621
Fabian Aryo <fabianaryo@gmail.com>
2020-03-23 19:47:16 -0600

    Added GUI for doctor

---
+++ Location.java
@@ -0,0 +1,12 @@
+package application;
+
+import java.util.ArrayList;
+
+public class Location {
+
+	private Department department;
+	private int room;
+	private ArrayList<User> staff;
+	
+	
+}
---
+++ User.java
@@ -0,0 +1,20 @@
+package application;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+	protected String name;
+	protected String emailAddress;
+	protected String phoneNumber;
+	
+
+	// constructor
+	public User(String name, String emailAddress, String phoneNumber) {
+		this.name = name;
+		this.emailAddress = emailAddress;
+		this.phoneNumber = phoneNumber;
+	}
+
+	
+}
